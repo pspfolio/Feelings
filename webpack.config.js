7cefb7e180464ef1088619f56c7fa97d45a3b8f9
pspfolio <pspfolio@gmail.com>
2016-06-17 17:25:43 +0300

    babel and webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: [
+    './app/app.js'
+  ],
+	module: {
+		loaders: [{
+		  test: /\.js?$/,
+		  exclude: /node_modules/,
+		  loader: 'babel'
+		}]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
