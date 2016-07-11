9f18de00000ccfd1d979b73ce217320d95277314
pspfolio <pspfolio@gmail.com>
2016-07-11 17:06:40 +0300

    feeling icons to project and add them to template

--- app/components/feeling/feeling.component.js
+++ app/components/feeling/feeling.component.js
@@ -4,11 +4,19 @@ const feelingComponent = {
     controller: controller,
     template: `
         <h1>Feeling</h1>
-        <ul>
-            <li ng-repeat="feeling in $ctrl.feelingRatings">
-                <span ng-bind="feeling.image" ng-click="$ctrl.feelingClicked(feeling.rating)"></span>
-            </li>
-        </ul>
+        <div class="feelingContainer">
+            <ul>
+                <li>
+                    <img src="images/sad.svg" alt="sad feeling"/>
+                </li>
+                <li>
+                    <img src="images/ok.svg" alt="sad feeling"/>
+                </li>
+                <li>
+                    <img src="images/happy.svg" alt="sad feeling"/>
+                </li>
+            </ul>
+        </div>
     `
 }
 
